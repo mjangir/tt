5dbf7ec9ee178217ed2d7acd4aeedc39bf77a292
mjangir <mjangir70@gmail.com>
2017-09-17 22:32:38 +0530

    fixed point number 5

--- server/sockets/events/jackpot/connect.js
+++ server/sockets/events/jackpot/connect.js
@@ -30,7 +30,14 @@ function emitCanIBidOnConnect(socket, jackpot, jackpotUser)
         jackpotLastBidUserId    = jackpotLastBidUser != null ? jackpotLastBidUser.getMetaData().id : false,
         jackpotUserId           = jackpotUser.getMetaData().id;
 
-    socket.emit(EVT_EMIT_CAN_I_PLACE_BID, {canIBid: (jackpotLastBidUserId != jackpotUserId)});
+    if(Object.keys(jackpot.users).length <= jackpot.getMetaData().minPlayersRequired)
+    {
+        global.jackpotSocketNamespace.in(jackpot.getRoomName()).emit(EVT_EMIT_CAN_I_PLACE_BID, {canIBid: false});
+    }
+    else
+    {
+        socket.emit(EVT_EMIT_CAN_I_PLACE_BID, {canIBid: (jackpotLastBidUserId != jackpotUserId)});
+    }
 }
 
 /**
